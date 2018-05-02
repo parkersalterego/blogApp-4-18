cca55e9f79ed385297a0d5207885db5236422392
parkersalterego <parkersalterego92@gmail.com>
2018-05-02 13:56:12 -0700

    front-end -- login added to home page and user service created

--- frontEnd/src/app/components/home/home.component.ts
+++ frontEnd/src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
