229e91e18e2b1bd841658ce236ed9923b0400ef0
parkersalterego <parkersalterego92@gmail.com>
2018-05-05 23:24:24 -0700

    login and register refactored to use template driven forms and flashmessages

--- frontEnd/src/app/components/login/login.component.ts
+++ frontEnd/src/app/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { UserService } from '../../services/user.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  email: string;
+  password: string;
 
-  constructor() { }
+  constructor(
+              private userService: UserService,
+              private flashMessage: FlashMessagesService,
+              private router: Router
+  ) { }
 
   ngOnInit() {
+    // this.userService.getAuth().subscribe(auth => {
+    //  if(auth) {
+    //    this.router.navigate(['/]);
+    // }
+    // });
+  }
+
+  onSubmit() {
+    this.userService.loginUser({email: this.email, password: this.password})
+      .subscribe(data => {
+        if (data) {
+          this.flashMessage.show('Success: Logged In', {cssClass: 'alert-success', timeout: 2000});
+          setTimeout(() => {
+            this.router.navigate(['/']);
+          }, 2000);
+        }
+      },
+      err => {
+        if (err.message !== undefined) {
+          this.flashMessage.show('Error: ' + err.message, {cssClass: 'alert-error', timeout: 4000});
+        } else {
+          this.flashMessage.show('Error: Unknown Error', {cssClass: 'alert-error', timeout: 4000});
+        }
+      }
+    );
   }
 
 }
